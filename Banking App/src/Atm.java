112e79956c7ca03d9f864c246a8c485d31066007
Ayush Rastogi <ayushrastogi689@gmail.com>
2022-12-05 16:28:05 +0530

    Created Atm class which extends MainMenu

---
+++ Banking App/src/Atm.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Atm extends MainMenu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
