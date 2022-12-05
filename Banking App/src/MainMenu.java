84ec897e1b3fee6e2717ff911db741a84a1c40b6
Ayush Rastogi <ayushrastogi689@gmail.com>
2022-12-05 17:41:36 +0530

    Created login method in MainMenu

--- Banking App/src/MainMenu.java
+++ Banking App/src/MainMenu.java
@@ -1,8 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.text.DecimalFormat;
 
 public class MainMenu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		DecimalFormat moneyFormat = new DecimalFormat("'$'###, ##0.00");
+		
+		HashMap<Integer, Integer> data = new HashMap<Integer, Integer>();
+		
+		public void getLogin() throws IOException{
+			
+		}
 
 	}
 
