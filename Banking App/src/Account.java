529b3584e7b98f73616fa032efb05592d9d7b746
Ayush Rastogi <ayushrastogi689@gmail.com>
2022-12-07 22:36:38 +0530

    Created Account class in the Application

---
+++ Banking App/src/Account.java
@@ -0,0 +1,13 @@
+import java.text.*;
+import java.util.*;
+
+public class Account 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		DecimalFormat moneyFormat = new DecimalFormat("'Rs '###, ##0.00");
+		
+		}
+
+}
